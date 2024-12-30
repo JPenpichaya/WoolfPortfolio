f25de5880e433f09c4ff2dc315fb7c0951a913c7
JPenpichaya <j.penpichaya@gmail.com>
2024-12-30 17:15:34 +0700

    Contact

---
+++ src/components/Section5.jsx
@@ -0,0 +1,15 @@
+const Section5 = () => {
+    return (
+        <section className="h-dvh w-dvw flex flex-col justify-center items-center bg-gradient-to-b from-[#000615] via-[#0747B5] to-[#000615] text-white">
+            <h1 className="text-5xl font-bold animate-glow">CONTACT</h1>
+            <div class="border-t-4 border-white my-4 w-52"></div>
+            <div className="flex flex-row justify-evenly space-x-4">
+                <img src="/LinkedIn.svg" />
+                <img src="/email.svg" />
+                <img src="/phone.svg" />
+            </div>
+        </section>
+    )
+}
+
+export default Section5
